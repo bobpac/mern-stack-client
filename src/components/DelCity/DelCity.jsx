e2a98798ec2b11052fff9ad33fe7efecd2da32b7
bobpac <141352390+bobpac@users.noreply.github.com>
2023-10-28 13:09:04 -0400

    Clean up DelCity to use state from App

--- src/components/DelCity/DelCity.jsx
+++ src/components/DelCity/DelCity.jsx
@@ -1,30 +1,14 @@
-import { useState, useEffect } from 'react'
-import * as citiesAPI from '../../../utilities/city-api'
 
-export default function DelCity({ user}) {
-  const [cities, setCities] = useState([]);
-  useEffect(function(){
-    async function getCities() {
-      const cities = await citiesAPI.getCities(user);
-      setCities(cities);
-    }
-    getCities();
-  },[]);
 
-  async function handleDelete(evt) {
-    const deletedCity = await citiesAPI.delCity(evt.target.id)
-    const updatedCities = cities.filter(city => city._id != deletedCity._id)
-    setCities(updatedCities)
-  }
+export default function DelCity({ handleDelCity , cities, user}) {
 
   return (
     <>
     {cities.map(city =>(
       <div key={city._id}>
-        {city.city}  , {city.state_code} <button onClick={handleDelete} id={city._id}> Delete </button>
+        {city.city}  , {city.state_code} <button onClick={handleDelCity} id={city._id}> Delete </button>
       </div> )   
     )}
-
     </>
   )
 }
