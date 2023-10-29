7773fa4e75f41c43c786fdc5abb71c14f34825ae
bobpac <141352390+bobpac@users.noreply.github.com>
2023-10-29 09:13:45 -0400

    Weather Forecast

--- src/components/ShowCity/ShowCity.jsx
+++ src/components/ShowCity/ShowCity.jsx
@@ -15,39 +15,82 @@ export default function ShowCity({ user }) {
     getCities();
   }, []);
 
+  function setDayOfTheWeek(arr) {
+    const dayOfTheWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const date = new Date();
+    const day = date.getDay();
+    for (let num = 0; num < 7; num++) {
+      arr[num].dayOfTheWeek = dayOfTheWeek[(day + num) % 7];
+    }
+  }
+
   async function handleShow(evt) {
     evt.preventDefault();
     const results = await citiesAPI.getWeatherForCity(evt.target.id);
     delete results.daily[7];
+    // insert the name of the day
+    setDayOfTheWeek(results.daily);
     setWeather(results.daily);
+    const cityNameId = document.getElementById('cityName');
+    cityNameId.innerText = results.location;
+
     console.log(results.daily);
   }
+
   function getIconPath(icon) {
     return `../../../public/images/${icon}@2x.png`;
   }
+
   return (
     <>
-      {cities.map((city) => (
-        <div key={city._id}>
-          {city.city} , {city.state_code}{" "}
-          <button onClick={handleShow} id={city._id}>
-            {" "}
-            Get Weather{" "}
-          </button>
-        </div>
-      ))}
-      <center><h1> 7-day Forecast </h1></center>
-      <div className="weatherContainer">
-        {weather.map((day) => (
-          <div className="weatherDay" key={day.key}>
-            <img src={getIconPath(day.weather[0].icon)}></img>
-            <br />
-            {/* {day.weather[0].description} <br /> */}
-            High: {day.temp.max} <br />
-            Low: {day.temp.min} <br />
-            <br />
+      <div className="row">
+        <div className="col s12 m4 l3">
+          <center>
+            <h1> Cities </h1>
+          </center>
+          <div className="cityContainer">
+            {cities.map((city) => (
+              <div className="cityNames" key={city._id}>
+                {city.city} , {city.state_code}{" "}
+                <button
+                  className="GetWeatherButton"
+                  onClick={handleShow}
+                  id={city._id}
+                >
+                  {" "}
+                  Get Weather{" "}
+                </button>
+              </div>
+            ))}
           </div>
-        ))}
+        </div>
+
+        <div className="col s12 m8 l9">
+          <center>
+            <h1 id="cityName"> 7-day Forecast </h1>
+          </center>
+          <div className="weatherContainer" key={99}>
+            {weather.map((day) => (
+              <div className="weatherDay" key={day.key}>
+                {day.dayOfTheWeek} <br />
+                <img src={getIconPath(day.weather[0].icon)}></img>
+                <br />
+                {/* {day.weather[0].description} <br /> */}
+                High: {Math.floor(day.temp.max)} <br />
+                Low: {Math.floor(day.temp.min)} <br />
+                <br />
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
