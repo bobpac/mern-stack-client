1c31bbff4abac2123b56c7ac2b09c5f9053ef4ec
bobpac <141352390+bobpac@users.noreply.github.com>
2023-10-29 11:46:34 -0400

    Fix NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,29 +1,35 @@
-import M from 'materialize-css';
-import { Link } from 'react-router-dom'
-import * as userService from '../../../utilities/user-services'
-import './NavBar.css'
+import M from "materialize-css";
+import { Link } from "react-router-dom";
+import * as userService from "../../../utilities/user-services";
+import "./NavBar.css";
 
 export default function NavBar({ user, setUser }) {
-
   function handleLogOut() {
-    userService.logOut()
-    setUser(null)
+    userService.logOut();
+    setUser(null);
   }
 
   return (
-    <nav className=' blue darken-1'>
-      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Welcome, {user.name}
-
-      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
-      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <Link to='/cities/new'>Add City</Link>
-
-      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
-      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <Link to='/cities/delete'>Delete City</Link>
-
-      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 
-      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <Link to='/cities'>Show Cities</Link>
-      &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <Link to='/' onClick={handleLogOut}>Log Out</Link>
-
+    <nav className=" blue darken-1">
+      <div className="nav-wrapper">
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Welcome, {user.name}
+        <ul id="nav-mobile" className="right">
+          <li>
+            <Link to="/cities/new">Add City</Link>
+          </li>
+          <li>
+            <Link to="/cities/delete">Delete City</Link>
+          </li>
+          <li>
+            <Link to="/cities">Show Cities</Link>
+          </li>
+          <li>
+            <Link to="/" onClick={handleLogOut}>
+              Log Out
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
