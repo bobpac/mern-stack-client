680584fc6e0d591e3ec8cea1f9076c15a478f236
bobpac <141352390+bobpac@users.noreply.github.com>
2023-11-05 11:10:47 -0500

    Try to fix issue of not being able to get forecast

--- utilities/user-api.js
+++ utilities/user-api.js
@@ -1,5 +1,6 @@
 import sendRequest from './send-request'
 const BASE_URL = 'https://weather-server-e4ww.onrender.com/users'
+// const BASE_URL = 'http://localhost:4741/users'
 
 export async function signUp(userData) {
     return sendRequest(BASE_URL, 'POST', userData)
